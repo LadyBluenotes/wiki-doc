2c61af73cc071a177749ae8c064e55641ca9cbb8
gerrardsarah <gerrardsarah@gmail.com>
2023-04-25 12:17:31 -0700

    update links

--- src/pages/in-progress/track-my-funds.tsx
+++ src/pages/in-progress/track-my-funds.tsx
@@ -73,9 +73,9 @@ export default function TrackMyFunds() {
               }}
             />
           </IconButton>
-          {/* <IconButton
+          <IconButton
             onClick={() => {
-              router.push("https://pokedex.sarahgerrard.me/");
+              router.push("https://trackmyfunds.app/");
             }}
             sx={{
               top: -6,
@@ -90,7 +90,7 @@ export default function TrackMyFunds() {
                 color: "primary.700",
               }}
             />
-          </IconButton> */}
+          </IconButton>
         </Typography>
       </div>
       <Typography
@@ -99,11 +99,7 @@ export default function TrackMyFunds() {
           ml: 1,
         }}
       >
-        Track My Funds is a personal finance application that allows users to
-        track their income, expenses, and what remains at the end of the month.
-        It is a full stack application that uses Next.js, TypeScript, MongoDB,
-        Tailwind CSS, and Next-Auth. The application is currently in development
-        and is not yet available for demo or use.
+        Track My Funds is a new personal finance app built with Next.js, TypeScript, MongoDB, Tailwind CSS, and Next-Auth that enables users to track their expenses, income, and investments. Although still in early development, new features are on the way to enhance the user experience.
       </Typography>
       <Divider
         sx={{
@@ -134,12 +130,18 @@ export default function TrackMyFunds() {
         level="body1"
         sx={{
           ml: 1,
+          mb:2,
         }}
       >
-        Track My Funds will be a full-stack application that enables users to
-        take control of their finances. Users will be able to create an account,
-        log in, and track their income, expenses, and what remains at the end of
-        the month. The MVP will include the following features:
+        Track My Funds is an innovative personal finance application that empowers users to take full control of their financial picture. It's a cutting-edge full-stack application built with the latest technologies, including Next.js, TypeScript, MongoDB, Tailwind CSS, and Next-Auth.
+        </Typography>
+        <Typography
+        level="body1"
+        sx={{
+          ml: 1,
+        }}
+      >
+        Although Track My Funds is still in its early development stages, it already offers a set of features that help users track their expenses and income, with investments and savings being added in the future. This app is constantly evolving and improving to provide users with the best financial tracking experience possible.
       </Typography>
       <Divider
         sx={{
@@ -179,7 +181,7 @@ export default function TrackMyFunds() {
           mb: 1,
         }}
       >
-        The following must be installed on your machine:
+        Track My Funds is a user-friendly personal finance application that is readily accessible to the public through its online link. While users are encouraged to use the app through this link, those who want to customize the app for their specific needs will need to set up the required environment variables to enable the application to run smoothly.
       </Typography>
       <List
         size="sm"
@@ -205,12 +207,6 @@ export default function TrackMyFunds() {
           </ListItemDecorator>
           Google Account for Next-Auth
         </ListItem>
-        <ListItem>
-          <ListItemDecorator>
-            <ArrowRight />
-          </ListItemDecorator>
-          Discord Account for Next-Auth
-        </ListItem>
       </List>
       <Typography
         id="install"
@@ -231,29 +227,25 @@ export default function TrackMyFunds() {
           ml: 2,
         }}
       >
-        This will be where I will explain how to install the application.
+        If you wish to run Track My Funds on your local machine, there are a few steps you'll need to follow to ensure proper installation and functionality. These steps include:
       </Typography>
-      {/* <List>
+      <List sx={{
+        overflow: "auto",
+      }}>
         <ListItem>
           <ListItemDecorator>1.</ListItemDecorator>
           <Typography
             level="body1"
-            sx={{
-              ml: 2,
-            }}
           >
             Clone the repository from GitHub.
             <br />
-            <code>git clone https://github.com/LadyBluenotes/the-poke-dex</code>
+            <code>git clone https://github.com/LadyBluenotes/track-my-funds</code>
           </Typography>
         </ListItem>
         <ListItem>
           <ListItemDecorator>2.</ListItemDecorator>
           <Typography
             level="body1"
-            sx={{
-              ml: 2,
-            }}
           >
             Install NPM packages
             <br />
@@ -262,18 +254,22 @@ export default function TrackMyFunds() {
         </ListItem>
         <ListItem>
           <ListItemDecorator>3.</ListItemDecorator>
+          <Typography level="body1">
+            Copy the contents of the .env.example file into a new file called
+            .env and add the appropriate values.
+          </Typography>
+        </ListItem>
+        <ListItem>
+          <ListItemDecorator>4.</ListItemDecorator>
           <Typography
             level="body1"
-            sx={{
-              ml: 2,
-            }}
           >
             Run the app
             <br />
-            <code>npm start</code>
+            <code>npm run start</code>
           </Typography>
         </ListItem>
-      </List> */}
+      </List>
       <Divider
         sx={{
           my: 2,
@@ -299,18 +295,7 @@ export default function TrackMyFunds() {
           mb: 2,
         }}
       >
-        This part will give a brief overview of the technologies used to build
-        the application. In addition, you will find some of the technologies
-        that are currently in-use listed below.
-      </Typography>
-      {/* <Typography
-        level="body1"
-        sx={{
-          ml: 1,
-          mb: 2,
-        }}
-      >
-       
+        Track My Funds is a modern personal finance application built with a range of cutting-edge technologies to offer users a seamless and intuitive experience. Next.js is a powerful React-based web framework that offers server-side rendering, static site generation, and other advanced features. Next.js enables efficient code writing and error handling, ensuring scalability and high performance. Paired with TypeScript, a language that adds static type checking and other features to JavaScript, it offered an easier way to write scalable and maaintanable code. In addition, TypeScript offered a more efficient development environment that would ensure fewer errors and better type safety.
       </Typography>
       <Typography
         level="body1"
@@ -319,7 +304,7 @@ export default function TrackMyFunds() {
           mb: 2,
         }}
       >
-        
+       MongoDB is a flexible and scalable NoSQL database that provides high availability and reliable storage. With MongoDB, Track My Funds can store user data securely and manage it efficiently.
       </Typography>
       <Typography
         level="body1"
@@ -328,8 +313,17 @@ export default function TrackMyFunds() {
           mb: 2,
         }}
       >
-        
-      </Typography> */}
+        Tailwind CSS is a utility-first CSS framework that allows for rapid styling and responsive design. It provides pre-built, customizable classes that can be easily applied to HTML elements. It made a great choice for building user interfaces quickly and efficiently, in addition to ensuring consistency among the entire application.
+      </Typography>
+      <Typography
+        level="body1"
+        sx={{
+          ml: 1,
+          mb: 2,
+        }}
+      >
+        Next-Auth is a flexible authentication and authorization library for Next.js applications. It provides a simple and secure way to handle user authentication, with support for various authentication providers like Google, Facebook, and GitHub. Next-Auth was chosen for this project as it can handle user authentication and authorizaation efficiently and securely. By leveraging this technology, it allowed me to focus more on building the application to do what I wanted it to in a secure environment.
+      </Typography>
       <Typography
         id="builtWith"
         level="h3"
@@ -390,16 +384,7 @@ export default function TrackMyFunds() {
           mb: 2,
         }}
       >
-        Here will talk about why I decided to build the project and what I
-        anticipate getting out of it.
-      </Typography>
-      {/* <Typography
-        level="body1"
-        sx={{
-          ml: 1,
-          mb: 2,
-        }}
-      >
+        Track My Funds was an exciting project for me, as it provided a challenging opportunity to build a full-stack application that I could use myself. As someone who looks to track my finances through a spreadsheet and could never find an application that was able to provide the same features I used, I wanted to create an application that would empower users to take control of their finances but didn't overwhelm them with complicated features. I also wanted to challenge myself as a developer by leveraging the technologies I've been using in smaller capacities, including Next.js, TypeScript, MongoDB, Tailwind CSS, and Next-Auth.
       </Typography>
       <Typography
         level="body1"
@@ -408,6 +393,7 @@ export default function TrackMyFunds() {
           mb: 2,
         }}
       >
+        In building Track My Funds, I aimed to create a single-page application that would dynamically update based on user interactions. I also wanted to ensure that the application was responsive and could be easily used on both desktop and mobile devices. I chose to use MongoDB to store and manage user data securely, and Next-Auth to handle user authentication and authorization. As they appeared to be the best pair to do the job. They introduced ease of use as well as security.
       </Typography>
       <Typography
         level="body1"
@@ -416,7 +402,8 @@ export default function TrackMyFunds() {
           mb: 2,
         }}
       >
-      </Typography> */}
+        Furthermore, I wanted to use my design skills to create a visually appealing and easy-to-use application that had a clear theme. By incorporating Tailwind CSS, I was able to create a clean and simple design that was both functional and aesthetically pleasing. Ultimately, Track My Funds is a personal finance application that I am proud to have created, and I hope it can be useful for others, as well.
+      </Typography>
       <Divider sx={{ my: 2 }} />
       <Typography
         id="learned"
@@ -438,16 +425,7 @@ export default function TrackMyFunds() {
           mb: 2,
         }}
       >
-        Anything I learned during the development of Track My Funds will be
-        talked about here.
-      </Typography>
-      {/* <Typography
-        level="body1"
-        sx={{
-          ml: 1,
-          mb: 2,
-        }}
-      >
+        Track My Funds was a valuable learning experience for me, allowing me to hone my skills in more advanced React concepts. Specifically, I was able to simplify more complex problems by breaking them down into smaller, more manageable pieces. I leveraged technologies such as Next Auth and TailwindCSS to keep things more consitent throughout the complication and take out some of the more heavy lifting.
       </Typography>
       <Typography
         level="body1"
@@ -456,7 +434,17 @@ export default function TrackMyFunds() {
           mb: 2,
         }}
       >
-      </Typography> */}
+        This application also furthered my understanding of APIs and reaffirmed the importance and value that they provide to a developer.  By using APIs, I can gain access to a wealth of data that can be used to create engaging and dynamic applications. Through this experience, I was able to refine my skills in handling data from APIs and make asynchronous requests and get more familiar with managing multiple calls in larger applications.
+      </Typography>
+      <Typography
+        level="body1"
+        sx={{
+          ml: 1,
+          mb: 2,
+        }}
+      >
+        Overall, working on Track My Funds allowed me to gain valuable experience with a range of technologies and refine my skills in developing full-stack applications.
+      </Typography>
       <Divider sx={{ my: 2 }} />
       <Typography
         id="acknowledge"
@@ -478,8 +466,7 @@ export default function TrackMyFunds() {
           mb: 2,
         }}
       >
-        Anything that inspired me throughout the development of the application
-        will be listed here.
+        This application was largely an inspired by the spreadsheets I use in my personal accounting. Outside of that, there was some help from the community I have on Twitter, in getting opinions on how to handle certain problems I was facing, such as authentication and styling. I appreciate those within my network that were available to answer the questions I had.
       </Typography>
       {/* <List size="sm">
         <ListItem>
@@ -509,9 +496,7 @@ export default function TrackMyFunds() {
           mb: 2,
         }}
       >
-        While this project is in production, this section will be utilized to
-        provide information on what features are currently in development and
-        which features have been implemented.
+        This section will be utilized to be used to provide insight into the projects progres, such as things that have been accomplished, features that are to be implemented, and ways optimizations will be integrated.
       </Typography>
       <Typography
         level="h4"
@@ -524,7 +509,7 @@ export default function TrackMyFunds() {
           },
         }}
       >
-        Features to be implemented
+        Upcoming features
       </Typography>
       <List
         size="sm"
@@ -536,21 +521,31 @@ export default function TrackMyFunds() {
           <ListItemDecorator>
             <CheckBoxOutlineBlank />
           </ListItemDecorator>
-          In dashboard, based on month display total income and expenses and
-          what is left over.
-        </ListItem>
-        
-        <ListItem>
-          <ListItemDecorator>
-            <CheckBoxOutlineBlank />
-          </ListItemDecorator>
-          Let user sort based on month, year, category, etc.
+          User editing - change email, username, name, picture.
         </ListItem>
         <ListItem>
           <ListItemDecorator>
             <CheckBoxOutlineBlank />
           </ListItemDecorator>
-          Creating an account using email (credentials in Next-Auth).
+          Export data to CSV.
+        </ListItem>
+        <ListItem>
+          <ListItemDecorator>
+            <CheckBoxOutlineBlank />
+          </ListItemDecorator>
+          Let user sort and filter based on month, year, category, etc.
+        </ListItem>
+        <ListItem>
+          <ListItemDecorator>
+            <CheckBoxOutlineBlank />
+          </ListItemDecorator>
+          Creating an account using email / username (credentials in Next-Auth).
+        </ListItem>
+        <ListItem>
+          <ListItemDecorator>
+            <CheckBoxOutlineBlank />
+          </ListItemDecorator>
+          Integrate other Next-Auth providers.
         </ListItem>
         <ListItem>
           <ListItemDecorator>
@@ -590,6 +585,13 @@ export default function TrackMyFunds() {
           ml: 2,
         }}
       >
+        <ListItem>
+          <ListItemDecorator>
+            <Done />
+          </ListItemDecorator>
+          In dashboard, based on month display total income and expenses and
+          what is left over.
+        </ListItem>
         <ListItem>
           <ListItemDecorator>
           <Done />
